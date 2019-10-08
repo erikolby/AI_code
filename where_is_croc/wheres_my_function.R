45ae7e54074ad647a28d11d72d2d62c4af0407ec
Erik Olby <erik.olby@gmail.com>
2019-10-09 01:53:24 +0200

    made progress

--- where_is_croc/wheres_my_function.R
+++ where_is_croc/wheres_my_function.R
@@ -1,7 +1,41 @@
 wheres_my_function <- function(moveInfo, readings, positions, edges, probs) {
-  #moveInfo$moves = c(sample(getOptions(positions[3], edges), 1), 0)
-  #return(moveInfo)
-  move_ranger <- function(my_node, edges, goal_node) {
+  
+  calc_transition_matrix <- function(edges) { 
+    transition_matrix <- matrix(rep(0,40*40),ncol=40)
+    for (i in 1:40) {
+      first_column_index <- which(edges[,1] == i)
+      second_column_index <- which(edges[,2] == i)
+      length_first_column <- length(first_column_index)
+      length_second_column <- length(second_column_index)
+      #for (j in 1:40) {
+      neighbour <- 0
+      if (length_first_column > 0) {
+        for (k in 1:length_first_column) {
+          neighbour <- edges[first_column_index[k],2]
+          neighbour_index_one <- which(edges[,1] == neighbour)
+          neighbour_index_two <- which(edges[,2] == neighbour)
+          transition_matrix[neighbour, i] <- 1/(length(neighbour_index_one)+length(neighbour_index_two))  
+        #if (edges[j,1] == i) {
+        #  first_column_neighbours = which(edges[,1] == edges[j,2])
+        #  second_column_neighbours = which(edges[,2] == edges[j,2])
+        #  transition_matrix[j,i]          
+        }  
+        # Find the transition probability from neighbour: 
+      #  if (length_first_column > 0) {
+       #   for (k in 1:length_first_column) {
+        #    neighbours[k] <- edges[k,2]
+            #output_transition_vector[j,i] <- (length(first_column_index)+length(second_column_index))/40
+      #  }
+       # }
+        #if (length_second_column > 0) {
+         # for (k in second_column_index) {
+          #}
+          }
+      }
+    }
+  
+    
+  closest_path <- function(my_node, edges, goal_node) {
     frontier <- list()
     visited <- list()
     current_path <- list()
@@ -15,7 +49,7 @@ wheres_my_function <- function(moveInfo, readings, positions, edges, probs) {
       length_first_column <- length(first_column_index)
       
       if (length_first_column > 0) {
-        for (i in 1:length(first_column_index)) {
+        for (i in 1:length_first_column) { 
           if (!(edges[first_column_index[i],2] %in% in_frontier)) {
             frontier <- append(frontier, list(list(position = edges[first_column_index[i], 2], 
                                       path = append(current_path, edges[first_column_index[i], 1]))))
@@ -28,7 +62,7 @@ wheres_my_function <- function(moveInfo, readings, positions, edges, probs) {
       length_second_column <- length(second_column_index)
       
       if (length_second_column > 0) {
-        for (i in 1:length(second_column_index)) {
+        for (i in 1:length_second_column) {
           if (!(edges[second_column_index[i], 1] %in% in_visited)) {
             frontier <- append(frontier, list(list(position = edges[second_column_index[i], 1], 
                                       path = append(current_path, edges[second_column_index[i], 2]))))
@@ -63,16 +97,52 @@ wheres_my_function <- function(moveInfo, readings, positions, edges, probs) {
     move_matrix <- matrix(rep(1,40*40),ncol=40)
     for (i in 1:40) {
       for (j in 1:40) {
-        move_matrix[i,j] <- move_ranger(i, edges, j)
+        move_matrix[i,j] <- closest_path(i, edges, j)
       }
     }
     return(move_matrix)
   }
   
+  
   # When it is the first turn, calculate the move_matrix and add it to the mem field. 
-  if (moveInfo$mem$status == 0) {
+  if (!(is.matrix(moveInfo$mem$move_matrix))) {
+    print("Calculating matrix...")
     moveInfo$mem$move_matrix <- calc_move_matrix(edges)
+    moveInfo$mem$probability_vector <- numeric(40)
   }
   
-  print("jeha")
+  # Create fresh probability vector if the status field is indicating a new game. (The first or any other).
+  if (moveInfo$mem$status == 0 || moveInfo$mem$status == 1) {
+    for (i in 1:40) {
+      if (i %in% positions) {
+        moveInfo$mem$probability_vector[i] <- 0
+      }
+      else {
+        moveInfo$mem$probability_vector[i] <- 1/37
+      }
+    }
+    moveInfo$mem$status = 2
+  }
+   
+  
+  
+  random_walker <- function(moveInfo, positions) {
+  the_goal <- sample(40,1)
+  move_1 <- moveInfo$mem$move_matrix[positions[3], the_goal]
+  move_2 <- sample(c(moveInfo$mem$move_matrix[move_1, the_goal],0),1)
+  moveInfo$moves <- c(move_1, move_2)
+  return(moveInfo)
+  }
+  
+  # If we want a random walker... 
+  return(random_walker(moveInfo, positions))
+  
+  
+  
+  
+  
+  
+  
+  
+  
   }
